30ac9c70b0c83739cd31a1e06a7b47028f2f7637
Francisco Mesa <francisco.mc.ids@outlook.com>
2025-11-13 23:04:09 -0400

    Add support for CORS preflight requests in index.php

--- server/public/index.php
+++ server/public/index.php
@@ -15,8 +15,13 @@ $method = $_SERVER["REQUEST_METHOD"] ?? "GET";
 
 header("Content-Type: application/json; charset=utf-8");
 header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
+if ($method === "OPTIONS") {
+    http_response_code(200);
+    exit;
+}
 
 $indexController = new IndexController();
 $authController = new AuthController(new UserService(new Connection()), new JWTService());
